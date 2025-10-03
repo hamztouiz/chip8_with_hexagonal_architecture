ab5690b9056615b74d5cbb79fae629fc989ca1df
Hamza <hamzatouizrat@yahoo.com>
2025-10-03 12:09:02 +0200

    module_2: SDL hello world

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
-
+#include <SDL2/SDL.h>
+#define WINDOWS_TITLE "hello world"
+#define WINDOWS_WIDTH  480
+#define WINDOWS_HEIGHT 640
 
 int main(int argc, char* argv[]) {
-    printf("Hello, World!\n");
+    if(SDL_Init( SDL_INIT_EVERYTHING ) != 0) 
+    {
+        printf("SDL_Init failed \n");
+        printf("%s", SDL_GetError());
+        return 1;
+    }
+    SDL_Window* win = SDL_CreateWindow( WINDOWS_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOWS_WIDTH, WINDOWS_HEIGHT, SDL_WINDOW_SHOWN );
+    if(win == NULL) 
+    {
+        printf("SDL_CreateWindow failed \n");
+        printf("%s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+    
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(win);
+    SDL_Quit();
     return 0;
 }
